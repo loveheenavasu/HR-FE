653fd48168ab1cf9a78f2817e9b727d50371104f
Vicky <vicky.js@zestgeek.com>
2023-04-05 12:47:57 +0530

    feat: Chakra UI theme setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import ChakraTheme from "@chakra-ui/theme";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider theme={ChakraTheme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
